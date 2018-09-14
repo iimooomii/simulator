cdf2228aa578fb3aef7b1fc7283b8619a88b82f6
tautvydasrck <33618328+tautvydasrck@users.noreply.github.com>
2018-09-14 20:31:30 +0300

    About page created

--- about.php
+++ about.php
@@ -5,7 +5,7 @@ include_once('includes/aside.inc.php');
 ?>
 <div class="col">
     <div class="row">
-        <div class="col-md-12  odds">
+        <div class="col-md-12 odds">
             <div class="row">
                 <div class="col-md-12 odds-h1 odds-h1-large">
                     <div class="mb-2">About project</div>
@@ -15,6 +15,18 @@ include_once('includes/aside.inc.php');
                 </div>
                 <img class="odds-flag odds-flag-large" src="" alt="">
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 about-content p-3">
+            <h3>Simulator usage</h3>
+            <p>
+                This project is made to simulate betting on real sport events based on real results without using real money.
+            </p>
+            <h3>Gambling risk</h3>
+            <p>
+                Sometimes gambling can turn into seriuos addiction.
+            </p>
         </div>
     </div>
 </div>
